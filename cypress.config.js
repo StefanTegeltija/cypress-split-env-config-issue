19192a4d039e8f7d8387ce7af0ef28e4b929f5f5
Stefan Tegeltija <stefan.tegeltija@casinogrounds.com>
2023-03-28 16:47:35 +0200

    gdsagdsa

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress');
 const cypressSplit = require('cypress-split');
 const cucumber = require('cypress-cucumber-preprocessor').default;
+const { readFileSync } = require('fs');
 
 module.exports = defineConfig({
   e2e: {
@@ -11,6 +12,13 @@ module.exports = defineConfig({
       cypressSplit(on, config);
 
       on('file:preprocessor', cucumber());
+      const envName = config.env.name;
+      const text = readFileSync(`./${envName}.json`);
+      const values = JSON.parse(text);
+
+      config.env = {
+        ...values.env,
+      };
       // IMPORTANT: return the config object
       return config;
     },
